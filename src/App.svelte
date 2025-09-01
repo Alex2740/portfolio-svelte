90465460ca48d52782691935c8adbbd45f297fbf
Valentin Plantard-Wahl <alex2740.mc@gmail.com>
2025-09-01 12:20:03 -0400

    First blockout

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,64 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  let activeTab = 'about'; // Initialize with the default active tab
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<div class="flex h-screen max-w-6xl mx-auto gap-4 p-4">
+  <div class="bg-pink-500 rounded-2xl w-3xs flex-none">
+    <div>
+      <div></div>
+      <h1>Valentin Plantard-Wahl</h1>
+      <span>Developper</span>
+    </div>
+    <hr />
+    <div>
+      mail
+      phone
+      birthday
+      location
+    </div>
+    <div>
+      github
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
+  <div class="bg-purple-500 rounded-2xl grow relative overflow-ellipsis">
+    <nav class="bg-blue-400 rounded-2xl float-right">
+      <button on:click={() => activeTab = 'about'}>About</button>
+      <button on:click={() => activeTab = 'resume'}>Resume</button>
+      <button on:click={() => activeTab = 'portfolio'}>Portfolio</button>
+      <button on:click={() => activeTab = 'contact'}>Contact</button>
+    </nav>
+    <main>
+      {#if activeTab === 'about'}
+        <h2>About me</h2>
+        <p class="text-pretty">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, aut ab nam sint culpa dolorum eaque natus accusamus vero placeat nostrum fugiat vel, aliquid obcaecati! Quidem veniam nisi consequatur sint.</p>
+        <h3>What I'm doing</h3>
+        <div>grid</div>
+        <h3>Testimonials</h3>
+        <div>carrousel testi</div>
+        <h3>Clients</h3>
+        <div>carrousel client</div>
+      {:else if activeTab === 'resume'}
+        <h2>Resume</h2>
+        <div>Education</div>
+        <div>Experience</div>
+        <div>Skill</div>
+        <div>loader for skill</div>
+      {:else if activeTab === 'portfolio'}
+        <h2>Portfolio</h2>
+        <div>Selector</div>
+        <div>Grid project</div>
+      {:else if activeTab === 'contact'}
+        <h2>Contact</h2>
+        <div>map</div>
+        <h3>Contact form</h3>
+        <div>contact form</div>
+      {/if}
+    </main>
   </div>
-
-  <p class="text-red-500">
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+</div>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  :root {
+    --main-width: calc();
   }
 </style>
